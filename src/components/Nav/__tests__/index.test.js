558f0274f80e92fafb1f8160964b419050cfc684
Simone Brin <simonebrin@gmail.com>
2022-11-04 09:38:44 -0400

    started adding Gallery

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
